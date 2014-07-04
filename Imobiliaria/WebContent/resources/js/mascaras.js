abf0ac5beb45bc86039b18e4cf1e25945b9287e4
DiegoBertelli <diegostefa9@hotmail.com>
2014-07-04 20:28:24 -0300

    Change-Id: Ie1af96125ea1074541f3781fa740ee199102af9e

--- Imobiliaria/WebContent/resources/js/mascaras.js
+++ Imobiliaria/WebContent/resources/js/mascaras.js
@@ -1,21 +1,8 @@
-jQuery(function($) {
-	try {
-		$(".data").mask("99/99/9999");
-	} catch (err) {
-
-	}
-
-	try {
-		$(".telefone").mask("(999) 999-9999");
-	} catch (err) {
-
-	}
-
-	try {
-
-		$(".cpf").mask("999-999-999.99");
-	} catch (err) {
-
-	}
-
+$a = jQuery.noConflict();
+$a(document).ready(function() {
+	$a("#data").mask("99/99/9999"); 
+	$a("#telefone").mask("(99) 999-9999");
+	$a("#cpf").mask("999-999-999.99");    
+	$a("#cep").mask("99.999-999"); 
+	$a("#cnpj").mask("99.999.999/9999-99"); 
 });
